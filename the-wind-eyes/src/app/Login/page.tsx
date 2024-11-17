1330ec4ffd8dcbba50510599edb2a35d6949befa
Vitor4818 <vitorg.martins4818@gmail.com>
2024-11-16 23:16:45 -0300

    Arrumando estilos da tela de login

--- the-wind-eyes/src/app/Login/page.tsx
+++ the-wind-eyes/src/app/Login/page.tsx
@@ -3,46 +3,41 @@ import styles from "./page.module.css";
 
 export default function Login() {
   return (
-    <>
-      <main>
-        <div className={styles.mainBackground}></div>
-
-        <div className={styles.divBack}>
-          <section className={styles.loginCenter}>
-            <div className={styles.divForms}>
-              <h1 className={styles.tittle}>ENTRAR</h1>
-              <input
-                type="text"
-                name=""
-                id="email "
-                className={styles.input}
-                placeholder="Email"
+    <body className={styles.body}>
+      
+    <main>
+      <section className={styles.loginCenter}>
+        <div className={styles.divForms}>
+          <h1 className={styles.tittle}>ENTRAR</h1>
+          <input
+            type="text"
+            id="email"
+            className={styles.input}
+            placeholder="Email"
+            />
+          <input
+            type="password"
+            className={styles.input}
+            placeholder="Senha"
+            />
+          <div className={styles.buttonsDiv}>
+            <Button
+              label="Entrar"
+              backgroundColor="#51E54F"
+              width="20rem"
+              height="3rem"
               />
-              <input
-                type="password"
-                className={styles.input}
-                placeholder="Senha"
+            <span>OU</span>
+            <Button
+              label="Cadastrar"
+              backgroundColor="#195C18"
+              width="20rem"
+              height="3rem"
               />
-
-              <div className={styles.buttonsDiv}>
-                <Button
-                  label="Entrar"
-                  backgroundColor="#51E54F"
-                  width="20rem"
-                  height="3rem"
-                />
-                <span>OU</span>
-                <Button
-                  label="Cadastrar"
-                  backgroundColor="#195C18"
-                  width="20rem"
-                  height="3rem"
-                />
-              </div>
-            </div>
-          </section>
+          </div>
         </div>
-      </main>
-    </>
+      </section>
+    </main>
+              </body>
   );
 }
