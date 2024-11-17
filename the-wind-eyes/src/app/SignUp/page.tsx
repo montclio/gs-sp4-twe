bd5e50d9a790a4b78a8c50d717b7644e2e9d683b
Vitor4818 <vitorg.martins4818@gmail.com>
2024-11-16 23:50:10 -0300

    Criando e estilizando tela de cadastro

---
+++ the-wind-eyes/src/app/SignUp/page.tsx
@@ -0,0 +1,53 @@
+import { Button } from "../components/Button/Button";
+import Input from "../components/Input/Input";
+import styles from "./page.module.css";
+
+export default function Login() {
+  return (
+    <body className={styles.body}>
+      
+    <main>
+      <section className={styles.loginCenter}>
+        <div className={styles.divForms}>
+          <h1 className={styles.tittle}>CADASTRAR</h1>
+          <Input 
+          type="text"
+          placeholder="Nome"/>
+
+        <Input 
+          type="text"
+          placeholder="Sobrenome"/>
+        <Input 
+          type="text"
+          placeholder="Email"/>
+        <Input 
+          type="text"
+          placeholder="Data de nascimento"/>
+        <Input 
+          type="text"
+          placeholder="CEP"/>
+        <Input 
+          type="password"
+          placeholder="Senha"/>
+          
+          <div className={styles.buttonsDiv}>
+            <Button
+              label="Entrar"
+              backgroundColor="#51E54F"
+              width="20rem"
+              height="3rem"
+              />
+            <span className={styles.span}>OU</span>
+            <Button
+              label="Cadastrar"
+              backgroundColor="#195C18"
+              width="20rem"
+              height="3rem"
+              />
+          </div>
+        </div>
+      </section>
+    </main>
+              </body>
+  );
+}
