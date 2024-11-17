97fa2f56bba0ff94f9f1b7be1f61eebf6d5caa94
Vitor4818 <vitorg.martins4818@gmail.com>
2024-11-17 01:13:31 -0300

    Criando e estilizando tela de ranking de empresas parceiras sustentaveis

---
+++ the-wind-eyes/src/app/RankingPartnersCompanies/page.tsx
@@ -0,0 +1,20 @@
+import RankingCard from "../components/RankingCard/RankingCard";
+import styles from "./page.module.css";
+
+export default function Login() {
+  return (
+    <body className={styles.body}>
+      <main className={styles.main}>
+        <h1 className={styles.tittle}>Ranking parceiros</h1>
+
+        <RankingCard companie="1. Greentech" percentual="89%" scoreWidth="89%" />
+        <RankingCard companie="2. Suzano" percentual="42%" scoreWidth="42%" />
+        <RankingCard companie="3. Siemens" percentual="32%" scoreWidth="32%" />
+        <RankingCard companie="4. Ambev" percentual="25%" scoreWidth="25%" />
+
+
+
+      </main>
+    </body>
+  );
+}
